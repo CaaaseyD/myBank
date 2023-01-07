4161bbd86c565b6214f401a99972e755c30c7346
CaaaseyD <baimaxsolution@gmail.com>
2023-01-07 21:01:37 +1100

    import previous work

---
+++ src/com/myBank/SavingAccount.java
@@ -0,0 +1,15 @@
+package com.myBank;
+
+public class SavingAccount extends Account{
+
+	//����
+	private double interestRate;
+	
+	public SavingAccount(double init_balance, double interestRate) {
+		super(init_balance);
+		this.interestRate = interestRate;
+	}
+
+	
+	
+}
